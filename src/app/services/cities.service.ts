427dd4cbdc7c258413f175e66f390b72043b1c2a
Yuriy Kuchevskiy <yurakuchevskiy@mail.ru>
2017-07-28 23:53:28 +0300

    Removed useless comments and logs

--- src/app/services/cities.service.ts
+++ src/app/services/cities.service.ts
@@ -13,32 +13,9 @@ export class CitiesService {
   ) { }
 
   getDefaultCities(): Promise<City[]> {
-    // var c = [
-    //   {
-    //     id: 1,
-    //     name: 'city1'
-    //   },
-    //   {
-    //     id: 2,
-    //     name: 'city2'
-    //   },
-    //   {
-    //     id: 3,
-    //     name: 'city3'
-    //   }
-    // ];
-    // return c;
-    var resp = this.http.get('http://localhost:53545/api/v1/cities')
+    return this.http.get('http://localhost:53545/api/v1/cities')
       .toPromise()
-      .then(response => {
-        console.log('resp: ', response);
-        console.log('json: ', response.json());
-        console.log('json.data: ', response.json().data);
-        console.log('json.data as city[]: ', response.json().data as City[]);
-        return response.json() as City[]
-      });
-
-    return resp;
+      .then(response => response.json() as City[]);
   }
 
 }
