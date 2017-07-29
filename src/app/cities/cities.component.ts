b7c6db1d60fb4db54715906e74b8307b7142fb15
Yuriy Kuchevskiy <yurakuchevskiy@mail.ru>
2017-07-29 15:59:26 +0300

    Finished default cities settings form

--- src/app/cities/cities.component.ts
+++ src/app/cities/cities.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import { CitiesService } from '../services/cities.service';
+
+import { City } from '../models/city';
+
 @Component({
   selector: 'app-cities',
   templateUrl: './cities.component.html',
@@ -7,9 +11,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CitiesComponent implements OnInit {
 
-  constructor() { }
+  cities: City[];
+  //citiesCopy: City[];
+  newCityName: string;
+  updateCityName: string;
+
+  constructor(
+    private citiesService: CitiesService
+  ) { }
 
   ngOnInit() {
+    this.getCities();
+  }
+
+  getCities() {
+    this.citiesService.getDefaultCities()
+      .then(cities => {
+        this.cities = cities;
+        this.newCityName = '';
+      });
+  }
+
+  deleteCity(city: City) {
+    //console.log('delete ', city);
+    this.citiesService.deleteDefaultCity(city).then(() => this.getCities());
+  }
+
+  updateCity(city: City) {
+    //city.Name = this.updateCityName;
+    //console.log('update ', city);
+    this.citiesService.updateDefaultCity(city).then(() => this.getCities()).catch(() => this.getCities());
+  }
+
+  addCity() {
+    //console.log('add ', this.newCityName);
+    let city = new City();
+    city.Name = this.newCityName;
+    this.citiesService.addDefaultCity(city).then(() => this.getCities()).catch(() => this.getCities());;
   }
 
 }
--- src/app/services/cities.service.ts
+++ src/app/services/cities.service.ts
@@ -9,14 +9,38 @@ import { host } from '../api/api-constants'
 @Injectable()
 export class CitiesService {
 
+  private headers: Headers = new Headers({'Content-Type': 'application/json'});
+
   constructor(
     private http: Http
   ) { }
 
   getDefaultCities(): Promise<City[]> {
-    return this.http.get(`${host}/api/v1/cities`)
+    return this.http
+      .get(`${host}/api/v1/cities`)
       .toPromise()
       .then(response => response.json() as City[]);
+  }
+
+  deleteDefaultCity(city: City): Promise<void> {
+    return this.http
+      .delete(`${host}/api/v1/cities/${city.Id}`, {headers: this.headers})
+      .toPromise()
+      .then(() => null);
+  }
+
+  addDefaultCity(city: City): Promise<void> {
+    return this.http
+      .post(`${host}/api/v1/cities`, JSON.stringify(city), {headers: this.headers})
+      .toPromise()
+      .then(() => null);
+  }
+
+  updateDefaultCity(city: City): Promise<void> {
+    return this.http
+      .put(`${host}/api/v1/cities`, JSON.stringify(city), {headers: this.headers})
+      .toPromise()
+      .then(() => null);
   }
 
 }
